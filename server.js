4720439ccc88858fdf8aba79d1e217a9ce303d4a
Sayan Das <poke19962008@gmail.com>
2016-02-15 17:04:30 +0000

    added router setup and express HTTP server

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 var http = require('http');
 var express = require('express');
 
-var router = express();
-var server = http.createServer(router);
+var app = express();
+
+// Routes Declarationsnpm
+var api = require("./routes/api/index.js");
 
 // Middlewares
-router.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
+app.use("/api", api);
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
+app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = process.env.IP || "0.0.0.0";
+  var port = process.env.PORT || 3000;
+  
+  console.log("Listening at ", addr + ":" + port);
 });
