6d5d12f4a201022e27d1933c9e1036ea843ba89d
Sayan Das <poke19962008@gmail.com>
2016-02-16 20:49:27 +0530

    added cookie/session middleware

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
-module.export = {
+module.exports = {
   'mongo': {
     'uri': 'mongodb://localhost:27017/edufix',
     'port': 27017,
+  },
+  'session': {
+    'expiration': 360000,
+    'key': 'iloveapple',
   }
-}
+};
--- routes/api/teacher/methods.js
+++ routes/api/teacher/methods.js
@@ -1,11 +1,11 @@
 exports.teacherQuery = function(teacherSchema) {
-
   
+
   return teacherSchema;
 };
 
 exports.teacherDetailQuery = function (teacherDetailSchema) {
 
-  
+
   return teacherDetailSchema;
 };
