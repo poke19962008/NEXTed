68a638f1ae1406e2a3929fb2f38a26cb18c6bf9b
Sayan Das <poke19962008@gmail.com>
2016-02-16 13:25:16 +0000

    added router console log

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var router = express.Router();
 
+router.use(function(req, res, next) {
+    console.log(req.method, req.url);
+    next(); 
+});
+
 router.get('/', function(req, res){
     res.send("hello");
 });
