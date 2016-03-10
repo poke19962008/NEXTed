c972f74c224b352d8903e50925d1a17dea901052
Sayan Das <poke19962008@gmail.com>
2016-03-10 20:33:45 +0530

    added login for student

--- routes/api/student/index.js
+++ routes/api/student/index.js
@@ -1,8 +1,8 @@
 var Student = require('./model').student;
-var StudentDetail = require('./model').StudentDetail;
+var StudentDetail = require('./model').studentDetail;
 
 var express = require('express');
-var router = expres.Router();
+var router = express.Router();
 
 router.use(function (req, res, next){
   var type = req.session.IDType;
@@ -23,3 +23,64 @@ function isLoggedIn (req, res, next){
 router.get('/', function(req, res){
   res.send('Welcome to Student API');
 });
+
+router.get('/login', function (req, res, next){
+
+    var password = req.query.password;
+    var ID = req.query.ID;
+
+    // TODO: Encrypt when database is encrypted
+    // password = auth.encrypt(password);
+    var student = new Student({
+      'studentID': ID,
+      'password': password
+    });
+
+    student.verifyStudent(function(err, doc){
+      if(doc.length == 0) res.send({ 'status': 'failed' });
+      else if(doc.length == 1) {
+        req.session.IDType = "student";
+        req.session.ID = ID;
+
+        res.send({ 'status': 'success' });
+      }
+      else res.send({ 'status': 'ise' });
+    });
+});
+
+/**
+** CREATE DUMMY USERS (GET)
+** Testing Purpose Only
+**/
+router.get('/createDummyUser', function (req, res){
+  var student = new Student({
+    studentID: '123testID',
+    schoolID: '123SchoolID',
+    name: {
+      fName: 'Sayn',
+      mNae: 'Kr.',
+      lName: 'Das'
+    },
+    classes: [{
+      class_: 'X'
+    }],
+    password: '123test',
+  });
+
+  var studentDetail =new StudentDetail({
+     studentID: '123testID',
+     schoolID: '123SchoolID'
+  });
+
+
+  studentDetail.save(function (err){
+    if(err) console.log(err);
+  });
+  student.save(function(err){
+    if(err) console.log(err);
+  });
+
+  res.send('Added Successfully');
+});
+
+module.exports = router;
--- routes/api/student/methods.js
+++ routes/api/student/methods.js
@@ -1,4 +1,22 @@
+/**
+** All the methods for Student and StudentDetail models
+** Author: SAYAN DAS
+**/
+
+
 exports.studentMethod = function(studentSchema) {
+
+  /**
+  ** Verify Teacher
+  ** Matches for `teacherID` and `password`
+  **/
+  studentSchema.methods.verifyStudent = function(cb){
+    return this.model('teacher').find({
+      'studentID': this.teacherID,
+      'password': this.password
+    },
+    'studentID', cb);
+  };
 
   return studentSchema;
 };
