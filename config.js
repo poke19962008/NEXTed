e3e3a7b0b4d6aa0e1ea86548f4eb0b1c9dc2eccf
Sayan Das <poke19962008@gmail.com>
2016-03-14 22:49:30 +0530

    initial profile page

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  
+
   'mongo': {
     'uri': 'mongodb://localhost:27017/edufix',
     'port': 27017,
   },
 
   'session': {
-    'expiration': 360000,
+    'expiration': 360000*60*60,
     'key': 'iloveapple',
   },
 
