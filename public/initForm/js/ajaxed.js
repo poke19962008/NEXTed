65c7d257bc533caecb85c8da0721851349d069c3
Saurabh Garg <saurabhgarg@Saurabhs-MacBook-Air.local>
2016-03-19 05:29:07 +0530

    init form update email , designation and bio completed

--- public/initForm/js/ajaxed.js
+++ public/initForm/js/ajaxed.js
@@ -1,14 +1,17 @@
 
 $(document).ready(function(){
 
-  $('#submitt').on('click',function() {
+  $('#submitt').on('click',function(e) {
+
+    e.preventDefault();
+    e.stopPropagation(); // only neccessary if something above is listening to the (default-)event too
 
     var designation = $('#q1').val();
     var biography = $('#q2').val();
     var email = $('#q3').val();
-    var degree = $('#q4').val();
-    var course = $('#q5').val();
-    var institute = $('#q6').val();
+    var qdegree = $('#q4').val();
+    var qcourse = $('#q5').val();
+    var qinstitute = $('#q6').val();
     var awardtitle =$('#q7').val();
     var date1 =$('#q8').val();
   	var awarddescription =$('#q9').val();
@@ -20,28 +23,52 @@ $(document).ready(function(){
   	var awarddate = date1.split('/');
   	var expdate = date2.split('/');
     console.log("Hello");
-
+  //
   	$.ajax({
   		url:"/api/teacher/update/email",
-  		data: {
+      type:'POST',
+      data: {
   			email: email
   		},
-  		method: 'POST'
-  	})
-  	.done(function(data){
-  		console.log(data);
-		console.log("Yes, email worked");
+  		success: function(data){
+        console.log("first ajax done");
 
-		$.ajax({
-	  		url:"/api/teacher/update/designation",
-	  		data: {
-	  			desig:designation
-	  		},
-	  		type: 'POST'
-	  	})
-	  	.done(function(data){
-			console.log("Yes,desig worked");
-		});
+    		$.ajax({
+    	  		url:"/api/teacher/update/designation",
+            type:'POST',
+            data: {
+    	  			desig:designation
+    	  		},
+            success: function(data){
+              console.log("second ajax done");
+
+              $.ajax({
+                url:"/api/teacher/update/bio",
+                type:'POST',
+                data: {
+                  bio:biography
+                },
+                success: function(data){
+                  console.log("third ajax done");
+                  $.ajax({
+                    url:"/api/teacher/update"
+                  })
+                }
+              })
+            }
+
+    	  	})
+
+
+      },
+      error: function(err){
+        console.log("The error is "+ err);
+      },
+      complete: function(){
+        console.log("All requests completed");
+      }
+  	})
+
 
 	});
 
@@ -63,8 +90,6 @@ $(document).ready(function(){
   });
 
 
-
-});
 
   //
   //
