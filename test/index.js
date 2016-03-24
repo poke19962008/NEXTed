2c7fc59c21b4a269fed0296ecabc165b6cff8fad
Sayan Das <poke19962008@gmail.com>
2016-03-25 02:03:35 +0530

    CI bug fix

--- test/index.js
+++ test/index.js
@@ -5,13 +5,21 @@ var addr = process.env.IP || "0.0.0.0";
 var port = process.env.PORT || 3000;
 var server = supertest.agent(addr + ":" + port);
 
-var app = require("../server").app;
-
-
 describe("Sample API test", function(){
-  it("Should return Welcome", function(done){
-    server
-      .get('/api')
-      .expect(200, done) // THis is HTTP response
+  var app = require('../server');
+  beforeEach(function(){
+    app.listen(3000);
   });
+
+  it("GET /", function(){
+    server
+    .get("/")
+    .expect(200) // THis is HTTP response
+    .end(function(err,res){
+      res.status.should.equal(200);
+      res.body.error.should.equal(false);
+      done();
+    });
+  });
+
 });
